c3106d070e8329ce349d71b5d6e4b5e75ff6bf98
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 14:32:26 +0200

    Create variables.tf

---
+++ environments/dev/storage/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "select which region you will use"
+  type        = string
+}
+
+variable "project_id" {
+  description = "select which project you will build on it"
+  type        = string
+}
