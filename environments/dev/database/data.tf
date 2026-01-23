c6542fdf2d64441c078db8a215a0eb5094c77bcc
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 18:41:25 +0200

    Update data.tf

--- environments/dev/database/data.tf
+++ environments/dev/database/data.tf
@@ -1,6 +1,6 @@
 # 1. Reference the existing Subnet
-data "google_compute_subnetwork" "private-subnet" {
-  name    = "subnet-europe-west1"
+data "google_compute_network" "sql-vpc" {
+  name    = "	application-vpc"
   region  = var.region
   project = var.project_id
 }
