ca693409dc239315277ea58863fca45c3787fb26
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 21:03:21 +0200

    Update firewall.tf

--- environments/dev/networking/firewall.tf
+++ environments/dev/networking/firewall.tf
@@ -38,3 +38,19 @@ resource "google_compute_firewall" "allow_lb_hb" {
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
   target_tags   = ["allow-health-check"]
 }
+
+
+resource "google_compute_firewall" "allow_sql_access" {
+  name    = "allow-vm-to-sql"
+  network = google_compute_network.application_vpc.name
+  allow {
+    protocol = "tcp"
+    ports    = ["3306"]
+  }
+
+  # Target only your specific application VMs
+  source_tags = ["db-client"]
+  
+  # Destination is the range you allocated for Cloud SQL
+  destination_ranges = ["10.0.2.0/24"] 
+}
