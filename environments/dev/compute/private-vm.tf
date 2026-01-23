4cd3c58f809dc35a73d80641f76d20f29e234c74
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 15:44:55 +0200

    Update private-vm.tf

--- environments/dev/compute/private-vm.tf
+++ environments/dev/compute/private-vm.tf
@@ -27,3 +27,17 @@ resource "google_compute_instance" "private_vm" {
     scopes = ["https://www.googleapis.com/auth/devstorage.read_write", "https://www.googleapis.com/auth/logging.write"]
   }
 }
+
+
+# 2. Generate a Signed URL (Requirement 2)
+data "google_storage_object_signed_url" "get_url" {
+  bucket       = data.google_storage_bucket.ip_log_bucket.name
+  path         = "ip_info.txt"
+  duration     = "10m" # Valid for 10 minutes
+  http_method  = "GET"
+}
+
+output "signed_url" {
+  value       = data.google_storage_object_signed_url.get_url.signed_url
+  description = "The signed URL for the object (valid for 10 mins)"
+}
