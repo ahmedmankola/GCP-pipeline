4921721415529b5ef1770b11672ee59820b60c80
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 21:43:58 +0200

    Update variables.tf

--- environments/dev/compute/variables.tf
+++ environments/dev/compute/variables.tf
@@ -7,3 +7,12 @@ variable "project_id" {
   description = "select which project you will build on it"
   type        = string
 }
+
+variable "project_id" {
+  description = "refer to project number"
+  type        = string
+  default     = "644619680241"  
+}
+
+
+
