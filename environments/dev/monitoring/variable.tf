ed6db54fade3d79d57666e4594e03949bbe709db
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 21:31:39 +0200

    Update variable.tf

--- environments/dev/monitoring/variable.tf
+++ environments/dev/monitoring/variable.tf
@@ -1,1 +1,9 @@
+variable "region" {
+  description = "our main region"
+  type        = string
+}
 
+variable "project_id" {
+  description = "select which project you will build on it"
+  type        = string
+}
