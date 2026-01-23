e8e62cab9bb4b3393b754c9307c9b0124a34de6b
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 14:28:42 +0200

    Create private-vm.tf

---
+++ environments/dev/compute/private-vm.tf
@@ -0,0 +1,29 @@
+resource "google_compute_instance" "private_vm" {
+  name         = "private-ubuntu-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.private_subnet.id
+    # No access_config block = No Public IP
+  }
+
+  # Startup Script: Writes internal IP to the bucket
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    INTERNAL_IP=$(hostname -I | awk '{print $1}')
+    echo "My Internal IP is: $INTERNAL_IP" > ip_info.txt
+    gsutil cp ip_info.txt gs://${google_storage_bucket.ip_log_bucket.name}/vm_internal_ip.txt
+  EOT
+
+  service_account {
+    # Ensure the VM has scopes to write to Cloud Storage
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_write", "https://www.googleapis.com/auth/logging.write"]
+  }
+}
