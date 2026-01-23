5f9f0b3550762e940f43c140d21be7d23be273a4
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 21:06:12 +0200

    Create function.tf

---
+++ environments/dev/compute/function.tf
@@ -0,0 +1,31 @@
+# 1. Cloud Function 2nd Gen
+resource "google_cloudfunctions2_function" "metadata_logger" {
+  name        = "storage-metadata-logger"
+  location    = "europe-west1"
+  description = "Logs filename and size on upload"
+
+  build_config {
+    runtime     = "python311"
+    entry_point = "log_file_metadata"
+    source {
+      storage_source {
+        bucket = google_storage_bucket.source_code_bucket.name
+        object = google_storage_bucket_object.function_zip.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256Mi"
+  }
+
+  event_trigger {
+    event_type    = "google.cloud.storage.object.v1.finalized"
+    trigger_region = "europe-west1" # Must match bucket location
+    event_filters {
+      attribute = "bucket"
+      value     = google_storage_bucket.trigger_bucket.name
+    }
+  }
+}
