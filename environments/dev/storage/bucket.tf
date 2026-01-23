f0ebebbb416e1de82c7aeef8d51509b4ef9f08e1
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 14:36:13 +0200

    Update bucket.tf

--- environments/dev/storage/bucket.tf
+++ environments/dev/storage/bucket.tf
@@ -1,11 +1,7 @@
 # 1. Storage Bucket to hold the IP file
 
-resource "random_id" "bucket_suffix" {
-  byte_length = 4
-}
-
 resource "google_storage_bucket" "ip_log_bucket" {
-  name          = "vm-internal-ip-log-${random_id.bucket_suffix.hex}"
+  name          = "vm-internal-ip-log"
   location      = var.region
   project       = var.project_id
   force_destroy = true
