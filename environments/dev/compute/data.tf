12e2e89871017ed5412b8233dea100ba6ce2a230
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-23 14:35:33 +0200

    Create data.tf

---
+++ environments/dev/compute/data.tf
@@ -0,0 +1,11 @@
+# 1. Reference the existing Subnet
+data "google_compute_subnetwork" "VM-private_subnet" {
+  name    = "your-existing-subnet-name"
+  region  = var.region
+  project = var.project_id
+}
+
+# 2. Reference the existing Storage Bucket
+data "google_storage_bucket" "ip_log_bucket" {
+  name = "your-existing-bucket-name"
+}
