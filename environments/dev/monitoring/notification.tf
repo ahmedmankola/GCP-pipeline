fbb8bfd5f46825dfe36aa80f8a5e050dd22ec5fe
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 10:58:52 +0200

    Update notification.tf

--- environments/dev/monitoring/notification.tf
+++ environments/dev/monitoring/notification.tf
@@ -2,6 +2,6 @@ resource "google_monitoring_notification_channel" "email_me" {
   display_name = "On-Call Email"
   type         = "email"
   labels = {
-    email_address = "ahmed.mankola@orange.com"
+    email_address = "ahmed.mankola@gmail.com"
   }
 }
