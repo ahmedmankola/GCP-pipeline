ba5eb8af279da0ea76976ebd42f6f737de52c72a
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 10:10:42 +0200

    Create uptime.tf

---
+++ environments/dev/monitoring/uptime.tf
@@ -0,0 +1,25 @@
+resource "google_monitoring_uptime_check_config" "lb_reachability" {
+  display_name = "LB-Reachability-Check"
+  timeout      = "10s"
+  period       = "60s"
+  log_check_failures = true
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = var.project_id
+      # This pulls the IP address from the data block
+      host       = data.google_compute_global_address.lb_ip.address
+    }
+  }
+
+  http_check {
+    path         = "/"
+    port         = "80"
+    request_method = "GET"
+    
+    # Optional: ensure your LB is returning a healthy 200 code
+    accepted_response_status_codes {
+      status_class = "STATUS_CLASS_2XX"
+    }
+  }
+}
