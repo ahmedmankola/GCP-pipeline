d74f5914d4d139e9520ab21172ae956c3812a906
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 16:48:51 +0200

    Update main.tf

--- environments/dev/database/main.tf
+++ environments/dev/database/main.tf
@@ -3,7 +3,8 @@ resource "google_sql_database_instance" "mysql_private" {
   name             = "mysql-private-instance"
   region           = "europe-west1"
   database_version = "MYSQL_8_0"
-  
+  # 1. ALLOW TERRAFORM TO DELETE (Meta-parameter)
+  deletion_protection = false
 
   settings {
     tier = "db-f1-micro" # Choose appropriate size
