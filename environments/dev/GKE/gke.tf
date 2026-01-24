1b45f3016c8cf00f39b63921f3c7d6ac7db585da
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 10:39:42 +0200

    Update gke.tf

--- environments/dev/GKE/gke.tf
+++ environments/dev/GKE/gke.tf
@@ -5,6 +5,7 @@ resource "google_container_cluster" "primary" {
   # We delete the default pool to use a separately managed one
   remove_default_node_pool = true
   initial_node_count       = 1
+  network    = data.google_compute_network.gke-private_subnet.id
   subnetwork = data.google_compute_subnetwork.gke-private_subnet.id
 }
 
