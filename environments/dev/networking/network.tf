d1f31775cab1eec432d5d8dbf9ccae706b3819f6
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 11:45:05 +0200

    Update network.tf

--- environments/dev/networking/network.tf
+++ environments/dev/networking/network.tf
@@ -26,7 +26,26 @@ resource "google_compute_subnetwork" "subnet_B" {
   private_ip_google_access = true
 }
 
-# 1. Allocate a private IP range for Google Managed Services
+resource "google_compute_subnetwork" "gke-private_subnet" {
+  name          = "gke-subnet-europe-west1"
+  ip_cidr_range = "10.0.3.0/24" # Your main node range
+  region        = "europe-west1"
+  network       = data.google_compute_network.gke-vpc.id
+
+  # ADD THESE BLOCKS
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "172.1.0.0/16" # Range for your Pods
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "172.2.0.0/20" # Range for your Services
+  }
+}
+
+
+# Allocate a private IP range for Google Managed Services
 resource "google_compute_global_address" "private_ip_alloc" {
   name          = "google-managed-services-range"
   purpose       = "VPC_PEERING"
@@ -36,10 +55,9 @@ resource "google_compute_global_address" "private_ip_alloc" {
   network       = google_compute_network.application_vpc.id # Use your VPC data block
 }
 
-# 2. Create the Private Service Connection (Peering)
+# Create the Private Service Connection (Peering)
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.application_vpc.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_alloc.name]
 }
-
