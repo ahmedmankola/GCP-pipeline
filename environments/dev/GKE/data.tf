3ae6d8c0d9ab770d49e555b5a9f981bf310afd5e
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 10:40:13 +0200

    Update data.tf

--- environments/dev/GKE/data.tf
+++ environments/dev/GKE/data.tf
@@ -4,3 +4,9 @@ data "google_compute_subnetwork" "gke-private_subnet" {
   region  = var.region
   project = var.project_id
 }
+
+# 1. Reference the existing Subnet
+data "google_compute_network" "gke-vpc" {
+  name    = "application-vpc"
+  project = var.project_id
+}
