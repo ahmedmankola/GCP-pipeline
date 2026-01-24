97df0f5612a6e497036d31cdc0ef6f88e982828e
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 15:25:20 +0200

    Update cloudbuild.tf

--- environments/dev/GKE/cloudbuild.tf
+++ environments/dev/GKE/cloudbuild.tf
@@ -1,7 +1,7 @@
 resource "google_cloudbuild_trigger" "gke_deploy_trigger" {
   name     = "deploy-to-gke"
   location = "europe-west1" # Must match your repo/connection location
-
+  service_account = data.google_service_account.cloud_build_sa.id
   # Link to your 2nd Gen Repository resource
   repository_event_config {
     repository = "projects/${var.project_id}/locations/europe-west1/connections/pipeline-github-mankola/repositories/ahmedmankola-GCP-pipeline"
