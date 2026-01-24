95012bf7f872cd7df63c2b5fc0bf9bc562c916bf
ahmedmankola <ahmed.mankola@gmail.com>
2026-01-24 11:23:37 +0200

    Update gke.tf

--- environments/dev/GKE/gke.tf
+++ environments/dev/GKE/gke.tf
@@ -7,6 +7,17 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
   network    = data.google_compute_network.gke-vpc.id
   subnetwork = data.google_compute_subnetwork.gke-private_subnet.id
+private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false # Keep the Control Plane public so you can use kubectl
+    master_ipv4_cidr_block  = "172.16.0.0/28" # Internal range for the master
+  }
+
+  # Required for Private Clusters
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pods"
+    services_secondary_range_name = "services"
+  }
 }
 
 # 2. Managed Node Pool
@@ -14,8 +25,8 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "main-pool"
   location   = "europe-west1"
   cluster    = google_container_cluster.primary.name
-  node_count = 2
-
+  min_node_count = 1
+  max_node_count = 1 #
   node_config {
     preemptible  = true # Saves money for testing
     machine_type = "e2-medium"
